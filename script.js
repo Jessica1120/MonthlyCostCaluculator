7af7bb9bbbae6ec18a0996803bccd8013646d2cf
Jessica Brown <jessicakbrown1120@gmail.com>
2017-09-03 11:51:32 -0500

    object constructor created

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('am I here?');
+
+var employeeArray = [];
+function employeeData (firstNameIn, lastNameIn, idNumberIn, titleIn, annualSalaryin) {
+    this.firstName = firstNameIn;
+    this.lastName = lastNameIn;
+    this.idNumber = idNumberIn;
+    this.annualSalary = annualSalaryin;
+    employeeArray.push( this);
+} //object constructor creates employee using input data and pushes employee object into array
+
