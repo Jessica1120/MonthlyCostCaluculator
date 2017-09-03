7dedd8d13fcae3423831af353251d2d64ca03e60
Jessica Brown <jessicakbrown1120@gmail.com>
2017-09-03 12:36:48 -0500

    input form and add employee created

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 
 var employeeArray = [];
-function employeeData (firstNameIn, lastNameIn, idNumberIn, titleIn, annualSalaryin) {
+//put interactive functions here - calculate button
+
+function addEmployee() {
+    new Employee ($('#firstNameIn').val(), $('#lastNameIn').val(), $('idNumberIn').val(), $('#titleIn').val(), $('annualSalaryIn').val());
+    //new Employee added and pushed into an array
+}
+
+
+
+function Employee (firstNameIn, lastNameIn, idNumberIn, titleIn, annualSalaryIn) {
     this.firstName = firstNameIn;
     this.lastName = lastNameIn;
     this.idNumber = idNumberIn;
-    this.annualSalary = annualSalaryin;
+    this.title = titleIn;
+    this.annualSalary = annualSalaryIn;
     employeeArray.push( this);
 } //object constructor creates employee using input data and pushes employee object into array
 
